1dfc6917534a7114e623c78eadcbfdff3787574d
rajeshkumar karra <rajeshkumarkarra@outlook.com>
2020-04-03 18:05:41 +0530

    template added

--- enlight_life/settings.py
+++ enlight_life/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
