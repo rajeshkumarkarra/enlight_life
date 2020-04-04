a1d6a81ef35579656e71ff3990b1df6d57efb47a
rajeshkumar karra <rajeshkumarkarra@outlook.com>
2020-04-04 18:53:49 +0530

    jinja deleted

--- ngo/views.py
+++ ngo/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'ngo/index.html', {'name':'Rajeshkumar'})
+    return render(request, 'ngo/index.html')
 
