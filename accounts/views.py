5ab0f7e9cadf307662841360162ae1fc9c336511
Rajeshkumar Karra <rajeshkumarkarra@outlook.com>
2020-05-11 16:31:54 +0530

    youtube link changed

--- accounts/views.py
+++ accounts/views.py
@@ -41,7 +41,7 @@ def register(request):
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
                 return redirect('register')
-            elif User.objects.filter(email=email).exists():
+            elif User.objects.filter(email=email).exists(): 
                 messages.info(request, 'Email Taken')
                 return redirect('register')
             else:
